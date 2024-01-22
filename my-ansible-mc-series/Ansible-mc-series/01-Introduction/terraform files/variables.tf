5e3c5a67cf1851b907b451240177f770efe5ad7e
mooa2023 <142276822+mooa2023@users.noreply.github.com>
2024-01-22 12:38:35 -0600

    Add files via upload

---
+++ my-ansible-mc-series/Ansible-mc-series/01-Introduction/terraform files/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type    = string
+  default = "us-west-1"
+}
+
+variable "my_instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "my_key" {
+  type    = string
+  default = "Key33_3"
+}
+
+variable "os" {
+  type    = string
+  #default = "linux"
+}
